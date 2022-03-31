8aaecfde85a65c6be508b954650356e0d3da3c36
Wanderson López <loepz.wanderson@gmail.com>
2022-03-30 20:54:40 -0600

    Se agregó la propiedad para las imágenes

--- Landmarks/Landmark.swift
+++ Landmarks/Landmark.swift
@@ -6,10 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Landmark: Hashable, Codable {
     var id: Int
     var name: String
     var park: String
     var state: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
 }
