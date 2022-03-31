26bd49e4e546e72e042ef70c5f036e7671230ef2
Wanderson López <loepz.wanderson@gmail.com>
2022-03-30 21:48:59 -0600

    Se agregó un comentario

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -26,6 +26,8 @@ struct LandmarkRow: View {
     }
 }
 
+//  The code writed in a preview provider only
+//  changes what Xcode displays in the canvas.
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
         Group {
