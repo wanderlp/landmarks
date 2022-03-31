55a75a92428cb3c693f42aba51a70143590d1e71
Wanderson López <loepz.wanderson@gmail.com>
2022-03-30 21:32:32 -0600

    Se agregó la nueva vista llamada LandmarkRow

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Wanderson López on 30/03/22.
+//
+//  This view is a row for displaying details
+//  about each landmark. This row view stores
+//  information in a property for the landmark
+//  it displays.
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
