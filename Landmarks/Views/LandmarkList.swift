38f0678b18facc18624bc7c913b1879fd1fb4364
Wanderson López <loepz.wanderson@gmail.com>
2022-03-30 21:51:57 -0600

    Se agregó una lista que muestra dos elementos estáticos

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Wanderson López on 30/03/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
