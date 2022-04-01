b9c56b6838530f0a219181e4f697fbf4f7cc9cf5
Wanderson López <loepz.wanderson@gmail.com>
2022-03-31 21:05:41 -0600

    Se agrega el parámetro image para que volver dinámica la vista

--- Landmarks/Views/CircleImage.swift
+++ Landmarks/Views/CircleImage.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Image("turtlerock")
+        image
             .resizable(resizingMode: .stretch)
             .frame(width: 250.0, height: 250.0)
             .clipShape(Circle())
@@ -22,6 +24,6 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
