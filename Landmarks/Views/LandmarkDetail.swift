9b6098effdb7a254f4e589b4a26425a3494dc8c2
Wanderson López <loepz.wanderson@gmail.com>
2022-03-31 20:33:58 -0600

    Diseño para la vista LandmarkDetail

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct LandmarkDetail: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            MapView()
+                .frame(height: 300.0)
+                .ignoresSafeArea(edges: .top)
+            CircleImage()
+                .padding(.bottom, -130.0)
+                .offset(y: /*@START_MENU_TOKEN@*/-130.0/*@END_MENU_TOKEN@*/)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                .foregroundColor(.black)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        Spacer()
+                    Text("Park's State")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here")
+            }
+            .padding()
+            
+            Spacer()
+        }
     }
 }
 
