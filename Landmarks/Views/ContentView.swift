7634a5f1db6ed8949dd69c430f8db9e8d41b956e
Wanderson López <loepz.wanderson@gmail.com>
2022-03-31 20:35:08 -0600

    Se cambia el diseño de la vista ContentView para que muestre el list view que contiene a los landmark

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -14,35 +14,7 @@ import SwiftUI
 //  and layout.
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300.0)
-                .ignoresSafeArea(edges: .top)
-            CircleImage()
-                .padding(.bottom, -130.0)
-                .offset(y: /*@START_MENU_TOKEN@*/-130.0/*@END_MENU_TOKEN@*/)
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                .foregroundColor(.black)
-                HStack {
-                    Text("Joshua Tree National Park")
-                        Spacer()
-                    Text("Park's State")
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here")
-            }
-            .padding()
-            
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
