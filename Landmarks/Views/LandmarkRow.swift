6e24fe4ce6ca3e3dfdab78dbfba142080875ebea
Wanderson López <loepz.wanderson@gmail.com>
2022-04-01 12:27:01 -0600

    Se cambió el color de la estrella a amarillo

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -25,6 +25,7 @@ struct LandmarkRow: View {
             
             if landmark.isFavorite {
                 Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
             }
         }
     }
