c0e44ee938dc7d0d239ef6f94451ff303d7f9b3f
Wanderson López <loepz.wanderson@gmail.com>
2022-04-01 12:31:19 -0600

    Se eliminan los preview para distintos dispositivos

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -29,10 +29,6 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (3rd generation)", "iPhone XS Max", "iPad Pro (12.9-inch) (5th generation)"], id: \.self) {
-                deviceName in LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+        LandmarkList()
     }
 }
