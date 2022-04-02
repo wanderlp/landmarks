688f4d9685a82fb72065b2f5cfb598035c19a378
Wanderson López <loepz.wanderson@gmail.com>
2022-04-02 12:21:07 -0600

    Se creó la nueva vista que contendrá el botón para agregar favoritos

---
+++ Landmarks/Views/FavoriteButton.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Wanderson López on 2/04/22.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton()
+    }
+}
