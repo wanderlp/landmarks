b071e57f3e19b4a01775cf66ce77a8cace993443
Wanderson López <loepz.wanderson@gmail.com>
2022-04-04 17:46:03 -0600

    Se agregó la nueva vista con un path que dibujará el fondo de los badges

---
+++ Landmarks/Views/BadgeBrackground.swift
@@ -0,0 +1,26 @@
+//
+//  BadgeBrackground.swift
+//  Landmarks
+//
+//  Created by Wanderson López on 4/04/22.
+//
+
+import SwiftUI
+
+struct BadgeBrackground: View {
+    var body: some View {
+        //  Paths combine lines, curves, and other
+        //  drawing primitives to form more complex
+        //  shapes like the badge's hexagonal background.
+        Path { path in
+        
+        }
+        .fill(.black)
+    }
+}
+
+struct BadgeBrackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBrackground()
+    }
+}
