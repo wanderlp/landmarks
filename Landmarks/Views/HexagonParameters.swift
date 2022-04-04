849b41104bd2b494f2c5c4e2f7dd2bdc0fb7e872
Wanderson López <loepz.wanderson@gmail.com>
2022-04-04 17:38:19 -0600

    Se creó un arreglo con las posiciones de los segmentos

--- Landmarks/Views/HexagonParameters.swift
+++ Landmarks/Views/HexagonParameters.swift
@@ -19,4 +19,43 @@ struct HexagonParameters {
         let curve: CGPoint
         let control: CGPoint
     }
+    
+    static let segments = [
+        // 1
+        Segment(
+            line:    CGPoint(x: 0.60, y: 0.05),
+            curve:   CGPoint(x: 0.40, y: 0.05),
+            control: CGPoint(x: 0.50, y: 0.00)
+        ),
+        // 2
+        Segment(
+            line:    CGPoint(x: 0.05, y: 0.20),
+            curve:   CGPoint(x: 0.00, y: 0.30),
+            control: CGPoint(x: 0.00, y: 0.25)
+        ),
+        // 3
+        Segment(
+            line:    CGPoint(x: 0.00, y: 0.70),
+            curve:   CGPoint(x: 0.05, y: 0.80),
+            control: CGPoint(x: 0.00, y: 0.75)
+        ),
+        // 4
+        Segment(
+            line:    CGPoint(x: 0.40, y: 0.95),
+            curve:   CGPoint(x: 0.60, y: 0.95),
+            control: CGPoint(x: 0.50, y: 1.00)
+        ),
+        // 5
+        Segment(
+            line:    CGPoint(x: 0.95, y: 0.80),
+            curve:   CGPoint(x: 1.00, y: 0.70),
+            control: CGPoint(x: 1.00, y: 0.75)
+        ),
+        // 6
+        Segment(
+            line:    CGPoint(x: 1.00, y: 0.30),
+            curve:   CGPoint(x: 0.95, y: 0.20),
+            control: CGPoint(x: 1.00, y: 0.25)
+        )
+    ]
 }
