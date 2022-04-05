19d018c9bbda1fadaee2c69aea9eb70fc9ba45df
Wanderson López <loepz.wanderson@gmail.com>
2022-04-04 18:17:55 -0600

    Se agrega el degradado

--- Landmarks/Views/BadgeBrackground.swift
+++ Landmarks/Views/BadgeBrackground.swift
@@ -67,9 +67,30 @@ struct BadgeBrackground: View {
                     )
                 }
             }
-            .fill(.black)
+            .fill(.linearGradient(
+                Gradient(
+                    colors: [
+                        Self.gradientStart,
+                        Self.gradientEnd
+                    ]
+                ),
+                startPoint: UnitPoint(x: 0.5, y: 0.0),
+                endPoint:   UnitPoint(x: 0.5, y: 0.6)
+            ))
         }
     }
+    
+    static let gradientStart = Color(
+        red:   239.0 / 255,
+        green: 120.0 / 255,
+        blue:  221.0 / 255
+    )
+    
+    static let gradientEnd = Color(
+        red:   239.0 / 255,
+        green: 172.0 / 255,
+        blue:  120.0 / 255
+    )
 }
 
 struct BadgeBrackground_Previews: PreviewProvider {
