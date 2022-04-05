b834d5627c85ae211e18bb865c11019552f40763
Wanderson López <loepz.wanderson@gmail.com>
2022-04-04 18:00:29 -0600

    Se incluye el control dentro de un GeometryReader para que el tamaño pueda ser variable sin que se deforme la figura

--- Landmarks/Views/BadgeBrackground.swift
+++ Landmarks/Views/BadgeBrackground.swift
@@ -9,51 +9,58 @@ import SwiftUI
 
 struct BadgeBrackground: View {
     var body: some View {
-        //  Paths combine lines, curves, and other
-        //  drawing primitives to form more complex
-        //  shapes like the badge's hexagonal background.
-        Path { path in
-            //  Defining starting point to the path, assuming a
-            //  container with size 100 x 100px.
-            var width: CGFloat = 100.0
-            let height = width
-            path.move(
-                to: CGPoint(
-                    x: width * 0.95,
-                    y: height * 0.20 + HexagonParameters.adjustment
-                )
-            )
-            
-            //  Drawing the lines for each point of the shape data
-            //  to create a rough hexagonal shape.
-            HexagonParameters.segments.forEach { segment in
-                //  The addLine(to:) method takes a single
-                //  point and draws it. Successive calls to add
-                //  Line(to:) begin a line at the previous point
-                //  and cointinue to the new point.
-                path.addLine(
+        //  Wrap the path in a GeometryReader so the
+        //  badge can use the size of its containing view,
+        //  which defines the size instead of hard-coding
+        //  the value 100.
+        GeometryReader { geometry in
+            //  Paths combine lines, curves, and other
+            //  drawing primitives to form more complex
+            //  shapes like the badge's hexagonal background.
+            Path { path in
+                //  Defining starting point to the path, assuming a
+                //  container with size 100 x 100px.
+                //var width: CGFloat = 100.0
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                path.move(
                     to: CGPoint(
-                        x: width * segment.line.x,
-                        y: height * segment.line.y
+                        x: width * 0.95,
+                        y: height * 0.20 + HexagonParameters.adjustment
                     )
                 )
                 
-                //  Use the addQuadCurve(to:control:)
-                //  method to draw the Bézier curves for the
-                //  badge's corner
-                path.addQuadCurve(
-                    to: CGPoint(
-                        x: width * segment.curve.x,
-                        y: height * segment.curve.y
-                    ),
-                    control: CGPoint(
-                        x: width * segment.control.x,
-                        y: height * segment.control.y
+                //  Drawing the lines for each point of the shape data
+                //  to create a rough hexagonal shape.
+                HexagonParameters.segments.forEach { segment in
+                    //  The addLine(to:) method takes a single
+                    //  point and draws it. Successive calls to add
+                    //  Line(to:) begin a line at the previous point
+                    //  and cointinue to the new point.
+                    path.addLine(
+                        to: CGPoint(
+                            x: width * segment.line.x,
+                            y: height * segment.line.y
+                        )
                     )
-                )
+                    
+                    //  Use the addQuadCurve(to:control:)
+                    //  method to draw the Bézier curves for the
+                    //  badge's corner
+                    path.addQuadCurve(
+                        to: CGPoint(
+                            x: width * segment.curve.x,
+                            y: height * segment.curve.y
+                        ),
+                        control: CGPoint(
+                            x: width * segment.control.x,
+                            y: height * segment.control.y
+                        )
+                    )
+                }
             }
+            .fill(.black)
         }
-        .fill(.black)
     }
 }
 
