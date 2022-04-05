6a72dbc890b12502035b5d7da477495c313ab792
Wanderson <lopez.wanderson@gmail.com>
2022-04-05 17:04:05 -0600

    Se creó la nueva vista Badge

---
+++ Landmarks/Views/Badge.swift
@@ -0,0 +1,20 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Wanderson López on 5/04/22.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
