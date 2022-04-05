d408c7147a5c7372ef90529ad9e1e014e87ac62d
Wanderson López <loepz.wanderson@gmail.com>
2022-04-05 12:15:32 -0600

    Se creó la nueva vista BadgeSymbol

---
+++ Landmarks/Views/BadgeSymbol.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Wanderson López on 5/04/22.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
