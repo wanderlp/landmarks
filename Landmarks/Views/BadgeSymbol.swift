bf10a4ba75fc623ef3ead30a67904a0c2c84349b
Wanderson <lopez.wanderson@gmail.com>
2022-04-05 14:37:01 -0600

    Se dibujan las líneas del icono

--- Landmarks/Views/BadgeSymbol.swift
+++ Landmarks/Views/BadgeSymbol.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct BadgeSymbol: View {
+    
+    static let symbolColor = Color(
+        red: 79.0 / 255,
+        green: 79.0 / 255,
+        blue: 191.0 / 255
+    )
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            Path { path in
+                let width = min(geometry.size.width, geometry.size.height)
+                let height = width * 0.75
+                let spacing = width * 0.030
+                let middle = width * 0.5
+                let topWidth = width * 0.226
+                let topHeight = width * 0.488
+                
+                //  Draws the top portion of the symbol
+                path.addLines([
+                    CGPoint(x: middle, y: spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: spacing)
+                ])
+                
+                //  Draws the bottom portion of the symbol
+                path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
+                path.addLines([
+                    CGPoint(x: middle - topWidth, y: topHeight + spacing),
+                    CGPoint(x: spacing, y: height - spacing),
+                    CGPoint(x: width - spacing, y: height - spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight + spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
+                ])
+            }
+            .fill(Self.symbolColor)
+        }
     }
 }
 
