1b0c1c9a484cff8a94853246ce1d5125dde4eb2e
Wanderson <lopez.wanderson@gmail.com>
2022-04-07 17:03:23 -0600

    Se crea la nueva vista "ProfileHost"

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Wanderson López on 7/04/22.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
