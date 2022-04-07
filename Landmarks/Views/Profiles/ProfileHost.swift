574796484551ef68a6415f5f54980282a99dc2bb
Wanderson <lopez.wanderson@gmail.com>
2022-04-07 17:23:43 -0600

    Diseño para la vista "ProfileHost"

--- Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -11,8 +11,13 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack (alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
     }
 }
 
