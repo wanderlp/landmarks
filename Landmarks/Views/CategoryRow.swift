4c7f159698e3fb4b203ecec43686c5a58b05e146
Wanderson <lopez.wanderson@gmail.com>
2022-04-07 12:31:41 -0600

    Se agregan las propiedades de nombre y la lista de items

--- Landmarks/Views/CategoryRow.swift
+++ Landmarks/Views/CategoryRow.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct CategoryRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    
     static var previews: some View {
-        CategoryRow()
+        CategoryRow(
+            categoryName: landmarks[0].category.rawValue,
+            items: Array(landmarks.prefix(3))
+        )
     }
 }
