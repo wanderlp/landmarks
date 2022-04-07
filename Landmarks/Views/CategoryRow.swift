24f236eb26655786cd6660a60d9cbf8242564177
Wanderson <lopez.wanderson@gmail.com>
2022-04-07 12:25:47 -0600

    Se creó la nueva vista CategoryRow

---
+++ Landmarks/Views/CategoryRow.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Wanderson López on 7/04/22.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow()
+    }
+}
