66cd45ad9451962feadc046e6571cde330f4cf8b
Wanderson <lopez.wanderson@gmail.com>
2022-04-07 13:53:19 -0600

    Se creó la vista CategoryItem

---
+++ Landmarks/Views/CategoryItem.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Wanderson López on 7/04/22.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem()
+    }
+}
