b9de8dfb9b11ec367835478df7ce303a6ecd9cf6
Wanderson <lopez.wanderson@gmail.com>
2022-04-07 14:12:56 -0600

    Se agrega el modifier listRowInsets para que la imagen se vea más grande

--- Landmarks/Views/CategoryHome.swift
+++ Landmarks/Views/CategoryHome.swift
@@ -22,10 +22,12 @@ struct CategoryHome: View {
                     .scaledToFill()
                     .frame(height: 200)
                     .clipped()
+                    .listRowInsets(EdgeInsets())
                 
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets())
             }
             .navigationTitle("Featured")
         }
