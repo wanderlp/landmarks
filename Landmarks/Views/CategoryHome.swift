06f191d6ff0acc6da2147ecb222b702f88e937d3
Wanderson <lopez.wanderson@gmail.com>
2022-04-07 09:38:09 -0600

    Se agregó la vista CategoryHome

---
+++ Landmarks/Views/CategoryHome.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Wanderson López on 7/04/22.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
