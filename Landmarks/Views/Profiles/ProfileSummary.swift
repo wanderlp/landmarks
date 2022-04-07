4dbdb95d1c77d66cbaab27aa2e31f997199fe739
Wanderson <lopez.wanderson@gmail.com>
2022-04-07 17:06:53 -0600

    Se crea la nueva vista "ProfileSummary"

---
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by Wanderson López on 7/04/22.
+//
+//  The profile summary takes a Profile value
+//  rather than a binding to the profile because
+//  the parent view, ProfileHost, manages the
+//  state for this view.
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary()
+    }
+}
