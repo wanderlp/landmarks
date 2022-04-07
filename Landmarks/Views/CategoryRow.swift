3aa719e99b3c3194294bd03bfd4f541b7be89ba0
Wanderson <lopez.wanderson@gmail.com>
2022-04-07 13:51:52 -0600

    Se agrega el scrollView horizontal

--- Landmarks/Views/CategoryRow.swift
+++ Landmarks/Views/CategoryRow.swift
@@ -12,7 +12,21 @@ struct CategoryRow: View {
     var items: [Landmark]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        Text(landmark.name)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
     }
 }
 
@@ -22,7 +36,7 @@ struct CategoryRow_Previews: PreviewProvider {
     static var previews: some View {
         CategoryRow(
             categoryName: landmarks[0].category.rawValue,
-            items: Array(landmarks.prefix(3))
+            items: Array(landmarks.prefix(4))
         )
     }
 }
