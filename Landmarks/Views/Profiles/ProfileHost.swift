0c4a8cbf91fc722e2759151dafffcd4617925a65
Wanderson <lopez.wanderson@gmail.com>
2022-04-08 09:36:09 -0600

    Se agregó el environment EditMode

--- Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -11,6 +11,12 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    //  SwiftUI provides storage in the environment
+    //  for values you can access using the
+    //  @Environment property wrapper. Access the
+    //  editMode value to read or write the edit
+    //  scope.
+    @Environment(\.editMode) var editMode
     @State private var draftProfile = Profile.default
     
     var body: some View {
