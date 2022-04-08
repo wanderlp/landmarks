85715feaa24e3a7fc1eb0d7c4dce85e89b4fcb70
Wanderson <lopez.wanderson@gmail.com>
2022-04-08 09:56:04 -0600

    Diseño inicial de la vista ProfileEditor

--- Landmarks/Views/Profiles/ProfileEditor.swift
+++ Landmarks/Views/Profiles/ProfileEditor.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            HStack {
+                Text("Username").bold()
+                Divider()
+                TextField("Username", text: $profile.username)
+            }
+        }
     }
 }
 
 struct ProfileEditor_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileEditor()
+        ProfileEditor(profile: .constant(.default))
     }
 }
