4218e7959241275a9c9ef754adbdcdb40fc2fe2a
Wanderson <lopez.wanderson@gmail.com>
2022-04-08 10:23:58 -0600

    Se agregó el campo GoalDate

--- Landmarks/Views/Profiles/ProfileEditor.swift
+++ Landmarks/Views/Profiles/ProfileEditor.swift
@@ -10,6 +10,12 @@ import SwiftUI
 struct ProfileEditor: View {
     @Binding var profile: Profile
     
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+        return min...max
+    }
+    
     var body: some View {
         List {
             HStack {
@@ -31,6 +37,10 @@ struct ProfileEditor: View {
                     }
                     .pickerStyle(.segmented)
                 }
+            }
+            
+            DatePicker(selection: $profile.goalDate, in: dateRange, displayedComponents: .date) {
+                Text("Goal Date").bold()
             }
         }
     }
