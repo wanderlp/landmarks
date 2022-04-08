34562574cdb78aef70cc2507d34e6d6bbcea35a1
Wanderson <lopez.wanderson@gmail.com>
2022-04-07 18:29:47 -0600

    Se agrega un estilo a la lista para que se ajuste mejor a la pantalla

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -30,6 +30,7 @@ struct CategoryHome: View {
                 }
                 .listRowInsets(EdgeInsets())
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
             .toolbar {
                 Button {
